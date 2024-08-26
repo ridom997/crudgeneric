2266f3c0df2b50a12df4020ada6e1d94b8f2b2e6
Pedro Rios Dominguez <pedrorido97@gmail.com>
2024-08-26 17:32:47 -0500

    First commit

---
+++ src/main/java/test/java/com/generic_crud/GenericCrudApplication.java
@@ -0,0 +1,13 @@
+package test.java.com.generic_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/java/com/generic_crud/GenericCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.generic_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
